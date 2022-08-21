cdb1cb83daad1f2fa76b8918c2eaa9231a3183dd
Garrick Hill <garrickhill5@gmail.com>
2022-08-21 19:03:57 +0300

    10_14_read_write_access

--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -7,7 +7,13 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.jsonWithPassword;
+import static ru.javawebinar.topjava.UserTestData.user;
 
 class JsonUtilTest {
     private static final Logger log = LoggerFactory.getLogger(JsonUtilTest.class);
@@ -26,5 +32,16 @@ class JsonUtilTest {
         log.info(json);
         List<Meal> actual = JsonUtil.readValues(json, Meal.class);
         MEAL_MATCHER.assertMatch(actual, meals);
+    }
+
+    @Test
+    void writeOnlyAccess() {
+        String json = JsonUtil.writeValue(user);
+        System.out.println(json);
+        assertThat(json, not(containsString("password")));
+        String jsonWithPass = jsonWithPassword(user, "newPass");
+        System.out.println(jsonWithPass);
+        User user = JsonUtil.readValue(jsonWithPass, User.class);
+        assertEquals(user.getPassword(), "newPass");
     }
 }
